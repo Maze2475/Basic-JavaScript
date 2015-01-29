86d77722aa24099d112510c334269fac2e36cd01
Mazin Hussain <mazin.hussain9@gmail.com>
2015-01-29 23:18:27 +0530

    Dealing with Objects in JavaScript
    How to create objects and assign values to them in JavaScript.

---
+++ Objects.js
@@ -0,0 +1,7 @@
+var student = new Object(); //How to declare a new object;
+student.name = "Mazin"; //Assign a variable and a value to that variable in the object (object name.variable name=value)
+student.ID = "CB005203";
+student.age = 20;
+student.grade = "2nd year";
+
+alert ("The student is: "+student.name+".ID Number: "+student.ID+".Age: "+student.age+"T.he student is currently in: "+student.grade+".");
