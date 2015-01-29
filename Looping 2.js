ec3b5876c7ead4e82db08429e66f98d5405fd944
Mazin Hussain <mazin.hussain9@gmail.com>
2015-01-29 23:16:02 +0530

    JavaScript Looping 102
    Simple exercise about using a for loop to access all the values in an
    array and modifying them

---
+++ Looping 2.js
@@ -0,0 +1,9 @@
+var names = new Array("Angelise", "Inaho", "Inko", "Slaine","Yui");
+
+var length = names.length;
+
+for(var i=0;i<length;i++)
+{
+	names[i]= "<p>"+names[i]+"</p>";
+	alert(names[i]);
+}
